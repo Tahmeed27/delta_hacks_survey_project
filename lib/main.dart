181a476e3d3962ce6aa48ccd7cae1b57eff9e8e6
Dhruv Mittal <57159970+dhmi19@users.noreply.github.com>
2020-01-15 23:30:51 -0500

    trying to keep a hold of reading data from firestore

--- lib/main.dart
+++ lib/main.dart
@@ -1,205 +1,116 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(MaterialApp(
-  home: Home(),
-));
+//Firestore Plugins
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 
-class Home extends StatelessWidget {
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: Colors.grey[900],
-      appBar: AppBar(
-        backgroundColor: Colors.grey[850],
-        title: Row(
-          children: <Widget>[
-            Expanded(
-              flex: 1,
-              child: Text(
-                'Survey App',
-                style: TextStyle(
-                  color: Colors.white,
-                  fontWeight: FontWeight.w400,
-                ),
-              ),
-            ),
-            Expanded(
-              flex: 0,
-              child: IconButton(
-                icon: Icon(Icons.account_circle),
-                color: Colors.grey[400],
-                onPressed: (){},
-              ),
-            ),
-            Expanded(
-              flex: 0,
-              child: IconButton(
-                icon: Icon(Icons.assessment),
-                color: Colors.grey[400],
-                onPressed: (){},
-              ),
-            )
-          ],
-        ),
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        // This is the theme of your application.
+        //
+        // Try running your application with "flutter run". You'll see the
+        // application has a blue toolbar. Then, without quitting the app, try
+        // changing the primarySwatch below to Colors.green and then invoke
+        // "hot reload" (press "r" in the console where you ran "flutter run",
+        // or simply save your changes to "hot reload" in a Flutter IDE).
+        // Notice that the counter didn't reset back to zero; the application
+        // is not restarted.
+        primarySwatch: Colors.green,
       ),
-      body: Padding(
-        padding: const EdgeInsets.fromLTRB(6.0, 3.0, 6.0, 0),
-        child: Column(
-          children: <Widget>[
-            Row(
-              children: <Widget>[
-                Container(
-                  margin: const EdgeInsets.fromLTRB(8.0, 15.0, 3.0, 0.0),
-                  padding: const EdgeInsets.symmetric(vertical: 5.0, horizontal: 10.0),
-                  decoration: BoxDecoration(
-                      border: Border.all(color: Colors.grey)
-                  ),
-                  child: Text(
-                    'FOOD',
-                    style: TextStyle(
-                      color: Colors.grey,
-                      letterSpacing: 2.0,
-                    ),
-                  ),
-                ),
-                Container(
-                  margin: const EdgeInsets.fromLTRB(8.0, 15.0, 3.0, 0.0),
-                  padding: const EdgeInsets.symmetric(vertical: 5.0, horizontal: 10.0),
-                  decoration: BoxDecoration(
-                      border: Border.all(color: Colors.grey)
-                  ),
-                  child: Text(
-                    'SOCIAL',
-                    style: TextStyle(
-                      color: Colors.grey,
-                      letterSpacing: 2.0,
-                    ),
-                  ),
-                ),
-                Container(
-                  margin: const EdgeInsets.fromLTRB(8.0, 15.0, 3.0, 0.0),
-                  padding: const EdgeInsets.symmetric(vertical: 5.0, horizontal: 10.0),
-                  decoration: BoxDecoration(
-                      border: Border.all(color: Colors.grey)
-                  ),
-                  child: Text(
-                    'SCHOOL',
-                    style: TextStyle(
-                      color: Colors.grey,
-                      letterSpacing: 2.0,
-                    ),
-                  ),
-                ),
-                Container(
-                  margin: const EdgeInsets.fromLTRB(8.0, 15.0, 3.0, 0.0),
-                  padding: const EdgeInsets.symmetric(vertical: 5.0, horizontal: 10.0),
-                  decoration: BoxDecoration(
-                      border: Border.all(color: Colors.grey)
-                  ),
-                  child: Text(
-                    'SPORTS',
-                    style: TextStyle(
-                      color: Colors.grey,
-                      letterSpacing: 2.0,
-                    ),
-                  ),
-                ),
-              ],
-            ),
-            Divider(
-              height: 50.0,
-              color: Colors.grey,
-            ),
-            Container(
-              margin: const EdgeInsets.fromLTRB(8.0, 15.0, 3.0, 0.0),
-              padding: const EdgeInsets.symmetric(vertical: 5.0, horizontal: 10.0),
-              decoration: BoxDecoration(
-                  border: Border.all(color: Colors.grey),
-                  borderRadius: BorderRadius.all(Radius.circular(7.0))
-              ),
-              child: Column(
-                children: <Widget>[
-                  Padding(
-                    padding: const EdgeInsets.all(20.0),
-                    child: Text(
-                      'QUESTION',
-                      style: TextStyle(
-                        color: Colors.grey,
-                        letterSpacing: 2.0,
-                      ),
-                    ),
-                  ),
-                  Text(
-                    'How did you like the food at the cafeteria today?',
-                    style: TextStyle(
-                      color: Colors.grey,
-                      letterSpacing: 2.0,
-                    ),
-                  ),
-                  Row(
-                    children: <Widget>[
-                      Container(
-                        margin: const EdgeInsets.fromLTRB(8.0, 15.0, 3.0, 0.0),
-                        padding: const EdgeInsets.symmetric(vertical: 60.0, horizontal: 10.0),
-                        decoration: BoxDecoration(
-                          shape: BoxShape.rectangle,
-                          color: Colors.redAccent,
-                        ),
-                        child: Text(
-                          'Very Good',
-                          style: TextStyle(
-                            color: Colors.grey[200],
-                            letterSpacing: 1.0,
-                          ),
-                        ),
-                      ),
-                      Container(
-                        margin: const EdgeInsets.fromLTRB(8.0, 15.0, 3.0, 0.0),
-                        padding: const EdgeInsets.symmetric(vertical: 60.0, horizontal: 10.0),
-                        decoration: BoxDecoration(
-                          shape: BoxShape.rectangle,
-                          color: Colors.amberAccent[700],
-                        ),
-                        child: Text(
-                          'Not Good or Bad',
-                          style: TextStyle(
-                            color: Colors.grey[200],
-                            letterSpacing: 1.0,
-                          ),
-                        ),
-                      ),
-                      Container(
-                        margin: const EdgeInsets.fromLTRB(8.0, 15.0, 3.0, 0.0),
-                        padding: const EdgeInsets.symmetric(vertical: 60.0, horizontal: 10.0),
-                        decoration: BoxDecoration(
-                          shape: BoxShape.rectangle,
-                          color: Colors.blueAccent,
-                        ),
-                        child: Text(
-                          'Very Bad',
-                          style: TextStyle(
-                            color: Colors.grey[200],
-                            letterSpacing: 1.0,
-                          ),
-                        ),
-                      ),
-
-                    ],
-                  )
-                ],
-              ),
-
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: (){},
-        child: Icon(Icons.add),
-        backgroundColor: Colors.grey[800],
-      ),
+      home: MyHomePage(title: 'Flutter Demo Home Page'),
     );
-
   }
 }
 
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key key, this.title}) : super(key: key);
+
+  // This widget is the home page of your application. It is stateful, meaning
+  // that it has a State object (defined below) that contains fields that affect
+  // how it looks.
+
+  // This class is the configuration for the state. It holds the values (in this
+  // case the title) provided by the parent (in this case the App widget) and
+  // used by the build method of the State. Fields in a Widget subclass are
+  // always marked "final".
+
+  final String title;
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  int _counter = 0;
+
+  void _incrementCounter() {
+    setState(() {
+      // This call to setState tells the Flutter framework that something has
+      // changed in this State, which causes it to rerun the build method below
+      // so that the display can reflect the updated values. If we changed
+      // _counter without calling setState(), then the build method would not be
+      // called again, and so nothing would appear to happen.
+      _counter++;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+// This method is rerun every time setState is called, for instance as done
+    // by the _incrementCounter method above.
+    //
+    // The Flutter framework has been optimized to make rerunning build methods
+    // fast, so that you can just rebuild anything that needs updating rather
+    // than having to individually change instances of widgets.
+    return Scaffold(
+      appBar: AppBar(
+        // Here we take the value from the MyHomePage object that was created by
+        // the App.build method, and use it to set our appbar title.
+        title: Text(widget.title),
+      ),
+      body: StreamBuilder<QuerySnapshot>(
+        stream: Firestore.instance.collection('users').snapshots(),
+        builder: (context, AsyncSnapshot<QuerySnapshot> snapshot) {
+          if(!snapshot.hasData) return const Text("Loading...");
+
+          if (snapshot.hasError)
+            return new Text('Error: ${snapshot.error}');
+
+          switch (snapshot.connectionState) {
+            case ConnectionState.waiting:
+              return new Text('Loading...');
+            default:
+              return new Text(snapshot.data.documents.length.toString());
+
+          /*return Text(
+            snapshot.data.documents[0].toString(),
+            textAlign: TextAlign.center,
+            overflow: TextOverflow.ellipsis,
+            style: TextStyle(fontWeight: FontWeight.bold),
+          );*/
+          }
+        } // builder
+
+        //child: ListView.builder(
+          //itemExtent: 80.0,
+          //itemCount: _bandList.length,
+          //)
+
+      )
+
+
+    );
+  }
+}
+
+//Trying Commit
+//Trying to commit after fixing bunch of errors
+// Dhruv trying to change
+//ted trying
