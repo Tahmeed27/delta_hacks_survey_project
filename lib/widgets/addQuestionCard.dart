c5a413ffc7d33524390d08e1df12334c33605153
Tahmeed <tmostafiz@gmail.com>
2020-01-26 00:25:08 -0500

    Added dropdown menu in question card

--- lib/widgets/addQuestionCard.dart
+++ lib/widgets/addQuestionCard.dart
@@ -7,19 +7,19 @@ class AddQuestionForm extends StatefulWidget {
 }
 
 class _EmailRegsFormState extends State<AddQuestionForm> {
-
   final TextEditingController _questionController = TextEditingController();
   final TextEditingController _choiceoneController = TextEditingController();
   final TextEditingController _choicetwoController = TextEditingController();
   final TextEditingController _choicethreeController = TextEditingController();
 
-  void _submit(){
-    print('Question: ${_questionController.text}\nChoice 1: ${_choiceoneController.text}\nChoice 2: ${_choicetwoController.text}\nChoice 3: ${_choicethreeController}' );
+  void _submit() {
+    print(
+        'Question: ${_questionController.text}\nChoice 1: ${_choiceoneController.text}\nChoice 2: ${_choicetwoController.text}\nChoice 3: ${_choicethreeController}');
     // accessing TextController using given format
     //enter firebase function here
   }
 
-  List<Widget> _buildChildren(){
+  List<Widget> _buildChildren() {
     return [
       TextField(
         controller: _questionController,
@@ -28,17 +28,10 @@ class _EmailRegsFormState extends State<AddQuestionForm> {
           hintText: 'Enter QUESTION',
         ),
       ),
-      SizedBox(height: 20.0,),
-      new DropdownButton<String>(
-        items: <String>['Facebook', 'Google', 'Food', 'Education', 'Sports', 'Politics'].map((String value) {
-          return new DropdownMenuItem<String>(
-            value: value,
-            child: new Text(value),
-          );
-        }).toList(),
-        hint: Text('Select appropriate tag for the Question'),
-        onChanged: (_) {},
+      SizedBox(
+        height: 20.0,
       ),
+      Dropdown(),
       TextField(
         controller: _choiceoneController,
         decoration: InputDecoration(
@@ -52,7 +45,6 @@ class _EmailRegsFormState extends State<AddQuestionForm> {
           labelText: 'Choice 2',
           hintText: 'Enter Option Two',
         ),
-        obscureText: true,
       ),
       TextField(
         controller: _choicethreeController,
@@ -60,9 +52,10 @@ class _EmailRegsFormState extends State<AddQuestionForm> {
           labelText: 'Choice 3',
           hintText: 'Enter Option Three',
         ),
-        obscureText: true,
       ),
-      SizedBox(height: 100.0,),
+      SizedBox(
+        height: 100.0,
+      ),
       RaisedGradientButton(
         child: Text(
           'Submit Question',
@@ -75,6 +68,7 @@ class _EmailRegsFormState extends State<AddQuestionForm> {
       ),
     ];
   }
+
   @override
   Widget build(BuildContext context) {
     return Padding(
@@ -88,7 +82,39 @@ class _EmailRegsFormState extends State<AddQuestionForm> {
   }
 }
 
+class Dropdown extends StatefulWidget {
+  Dropdown({Key key}) : super(key: key);
+  @override
+  _DropdownState createState() => _DropdownState();
+}
 
-
-
-
+class _DropdownState extends State<Dropdown> {
+  List<String> _filters = ["Sports", "Facebook", "Google", "Food", "Education"];
+  String dropdownValue;
+  @override
+  Widget build(BuildContext context) {
+    return DropdownButton<String>(
+      hint: Text("Please choose category"),
+      value: dropdownValue,
+      icon: Icon(Icons.arrow_drop_down),
+      iconSize: 24,
+      elevation: 16,
+      style: TextStyle(color: Colors.green),
+      underline: Container(
+        height: 2,
+        color: Colors.greenAccent,
+      ),
+      onChanged: (String newValue) {
+        setState(() {
+          dropdownValue = newValue;
+        });
+      },
+      items: _filters.map<DropdownMenuItem<String>>((String value) {
+        return DropdownMenuItem<String>(
+          value: value,
+          child: Text(value),
+        );
+      }).toList(),
+    );
+  }
+}
